c2b1a0c6d4d79b4096e95e18c6016a3ddd605ef8
yonis <yoni.scheyer@gmail.com>
2021-01-12 23:35:09 +0200

    wip

--- GUI/app/components/TestUpsertModal/TestUpsertModal.tsx
+++ GUI/app/components/TestUpsertModal/TestUpsertModal.tsx
@@ -8,7 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import Slide from '@material-ui/core/Slide';
 import { TransitionProps } from '@material-ui/core/transitions';
 import { TextField, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
-import { Test, TEST_STATUS } from '../../models/Test.model';
+import { Test, TestModel, TEST_STATUS } from '../../models/Test.model';
 import styles from './TestUpsertModal.css'
 
 import ServiceStore from '../../services /store.service'
@@ -101,14 +101,12 @@ export default function FullScreenDialog(props:any) {
 
   const handleUserPick = (e:any) => {
     const {userActions, user} = getUserActions(e);
-    //setPortsPlaying({...portsPlaying, [test.id]:playingContainerInstance._port})
     setPickedUser(user.id)
     setPickedUserActions(userActions)
   }
 
   const handleUserPickSuite = (e:any) => {
     const {userActions, user} = getUserActions(e);
-    //setPortsPlaying({...portsPlaying, [test.id]:playingContainerInstance._port})
     setPickedUser(user.id)
     setPickedUserActions(userActions)
   }
@@ -131,8 +129,8 @@ export default function FullScreenDialog(props:any) {
   }
 
   const addTestToSuite = (e:any) => {
-    const test:Test = {
-      name:testName,
+    const test:TestModel = {
+      testName,
       userId:pickedUserId,
       actionId:pickedUserAction,
       schedule:{},
@@ -159,13 +157,65 @@ export default function FullScreenDialog(props:any) {
             </Toolbar>
           </AppBar>
           <div className={styles["modal-content-container"]}>
+          <br/>
              <div className={styles["test-name-container-suite"]}>
             <TextField disabled={false} 
              onChange={( e => setSuiteName(e.target.value)) } 
              label="Test suite name:" variant="outlined" style={{width:"100%", height:"45px"}} size="small"/>
              </div>
              <br/>
-            <div className={styles["test-name-container"]}>
+        <div className={styles["pick-schedule-container"]}>
+           {
+           suite.length === 0 ? null : suite.map((test:any) => {
+               return (
+                 <div>
+                  <br/>  <br/>
+                 <div className={styles["test-name-container"]}>
+                 <TextField disabled={false} 
+                  onChange={( e => setTestName(e.target.value)) } 
+                  label="Test name:" variant="outlined" style={{width:"70%", height:"45px"}} size="small"/>
+                </div> <br/>
+                <div className={classes.userActionSelectContainer}>
+                <div className={styles["pick-user-combobox-container"]}>     
+                <FormControl className={classes.formControl}>
+                 <InputLabel id="demo-simple-select-label">Select User:</InputLabel>
+                 <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={test.userId}
+                  onChange={handleUserPickSuite}>
+                  {
+                    !users ? null : Object.values(users).map((user:any)=>{
+                      return <MenuItem value={user.id}>{user.name}</MenuItem>
+                    })
+                  }
+                 </Select>
+               </FormControl>
+              </div>
+              <div className={styles["pick-action-combobox-container"]}>
+              <FormControl className={classes.formControl}>
+               <InputLabel id="demo-simple-select-label">Select Action:</InputLabel>
+               <Select
+                 labelId="demo-simple-select-label"
+                 id="demo-simple-select"
+                 value={test.actionId}
+                 onChange={handleActionPick}>
+                 {
+                   !pickedUserActions ? null : pickedUserActions.map((userAction:any)=>{
+                     return <MenuItem value={userAction.id}>{userAction.name}</MenuItem>
+                   })
+                 }
+               </Select>
+             </FormControl>
+             </div>
+             </div>
+           </div>  
+               )
+             })     
+         }
+        </div>
+        <br/><br/>
+        <div className={styles["test-name-container"]}>
             <TextField disabled={false} 
              onChange={( e => setTestName(e.target.value)) } 
              label="Test name:" variant="outlined" style={{width:"70%", height:"45px"}} size="small"/>
@@ -207,52 +257,6 @@ export default function FullScreenDialog(props:any) {
              <Button size="small" variant="outlined" color="primary" onClick={addTestToSuite}>ADD +</Button>
           </div>
         </div>     
-        <div className={styles["pick-schedule-container"]}>
-           {
-           suite.length === 0 ? null : suite.map((test:any) => {
-               return (
-                <div className={classes.userActionSelectContainer}>
-                <div className={styles["pick-user-combobox-container"]}>     
-                <FormControl className={classes.formControl}>
-                 <InputLabel id="demo-simple-select-label">Select User:</InputLabel>
-                 <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={test.userId}
-                  onChange={handleUserPickSuite}>
-                  {
-                    !users ? null : Object.values(users).map((user:any)=>{
-                      return <MenuItem value={user.id}>{user.name}</MenuItem>
-                    })
-                  }
-                 </Select>
-               </FormControl>
-              </div>
-              <div className={styles["pick-action-combobox-container"]}>
-              <FormControl className={classes.formControl}>
-               <InputLabel id="demo-simple-select-label">Select Action:</InputLabel>
-               <Select
-                 labelId="demo-simple-select-label"
-                 id="demo-simple-select"
-                 value={test.actionId}
-                 onChange={handleActionPick}>
-                 {
-                   !pickedUserActions ? null : pickedUserActions.map((userAction:any)=>{
-                     return <MenuItem value={userAction.id}>{userAction.name}</MenuItem>
-                   })
-                 }
-               </Select>
-             </FormControl>
-             </div>
-             <div className={styles["pick-action-combobox-container"]} >
-             <Button size="small" variant="outlined" color="primary" onClick={addTestToSuite}>ADD +</Button>
-             </div>
-           </div>  
-               )
-             })     
-         }
-        </div>
-        <br/><br/>
          <div className={styles["done-cancel-btns"]}>
          <Button size="small" variant="outlined" color="secondary" onClick={handleClose}>Cancel</Button>
          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
