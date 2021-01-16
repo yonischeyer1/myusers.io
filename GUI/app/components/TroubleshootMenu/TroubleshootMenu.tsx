87ea16aa77e79d3442fc9c980ec2083524567115
yonis <yoni.scheyer@gmail.com>
2021-01-16 12:46:03 +0200

    wip

--- GUI/app/components/TroubleshootMenu/TroubleshootMenu.tsx
+++ GUI/app/components/TroubleshootMenu/TroubleshootMenu.tsx
@@ -9,7 +9,6 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Slide from '@material-ui/core/Slide';
 import { TransitionProps } from '@material-ui/core/transitions';
-import { TextField, FormControl } from '@material-ui/core';
 import Accordion from '@material-ui/core/ExpansionPanel';
 import AccordionSummary from '@material-ui/core/ExpansionPanelSummary';
 import AccordionDetails from '@material-ui/core/ExpansionPanelDetails';
@@ -17,10 +16,8 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 //** Others **
 import ServiceStore from '../../services /store.service';
-import RecordingModal from '../RecordingModal/RecordingModal'
 import styles from './TroubleshootMenu.css';
 
-const serviceStore = new ServiceStore();
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -51,7 +48,7 @@ const useStyles = makeStyles((theme: Theme) =>
       },
       heading: {
         fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
+        fontWeight: theme.typography.fontWeightBold,
       },
   }),
 );
@@ -64,13 +61,10 @@ const Transition = React.forwardRef(function Transition(
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-let onceflag = false
+
 export default function FullScreenDialog(props:any) {
   const classes = useStyles();
-  const [openRecordingModal, setOpenRecordingMoal] = React.useState(false);
-  const [accountName, setAccountName] = React.useState(false);
-  const [loginURL, setLoginURL] = React.useState(false);
-  const { open, pickedAccount } = props;
+  const { open, pickedTest } = props;
 
 
   const handleClose = (e:any) => {
@@ -102,24 +96,31 @@ export default function FullScreenDialog(props:any) {
           <Typography className={classes.heading}>Dynamic snapshot</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
+            <div>
+            <Typography>
+             When a tag snapshot has chaging parts in the UI its confusing the snapshot matching enigne
+             You can use static masking wizard in order to cover those parts 
           </Typography>
+          <br/>
+          <Button variant="outlined" color="primary">Open Static Masking wizard</Button>
+          </div>
         </AccordionDetails>
       </Accordion>
       <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-        >
+          aria-controls="panel2a-content">
           <Typography className={classes.heading}>UI Change</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
+            <div>
+            <Typography>
+                The website UI has changed to this snapshot.
+                Replace current tag image with the faild tag image.
+            </Typography>
+            <br/>
+          <Button variant="outlined" color="primary">Replace tag snapshot</Button>
+            </div>
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -127,13 +128,36 @@ export default function FullScreenDialog(props:any) {
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
         >
-          <Typography className={classes.heading}>Bug / Preformence issue</Typography>
+          <Typography className={classes.heading}>Performance issue / slowness</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
+            <div>
+            <Typography>
+                You can increase the wait time of each tag and number of attempts
+            </Typography>
+            <br/>
+          <Button variant="outlined" color="primary">Increase tag wait time</Button>
+            </div>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+        >
+          <Typography className={classes.heading}>Bug</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <div>
+            <Typography>
+                As a QA your point of refernce to check that the website behaves as it should is
+                the Features user stories.
+                <br/>
+                When you do detect an exepction in behaviour you should report it to a ticketing system.
+            </Typography>
+            <br/>
+          <Button variant="outlined" color="primary">Report Bug</Button>
+            </div>
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -144,10 +168,14 @@ export default function FullScreenDialog(props:any) {
           <Typography className={classes.heading}>A/B Test</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
+         <div>
+            <Typography>
+                Thier can be multiple themes for the same interface but elements you did an action on stay in thier postion.<br/>
+                You can add this snapshot to the tag and it will check to see of one of the options is correct
+            </Typography>
+            <br/>
+          <Button variant="outlined" color="primary">Add snaphsot to tag</Button>
+        </div>
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -158,10 +186,14 @@ export default function FullScreenDialog(props:any) {
           <Typography className={classes.heading}>Skip</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
+         <div>
+            <Typography>
+                You can skip tag snapshot matching (Be carful with this option it can lead to unexpeceted behaviour). 
+                
+            </Typography>
+            <br/>
+          <Button variant="outlined" color="primary">Skip tag</Button>
+         </div>
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -172,10 +204,15 @@ export default function FullScreenDialog(props:any) {
           <Typography className={classes.heading}>Live Snapshot</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
+        <div>
+            <Typography>
+                Add this as a live snapshot tag.<br/>
+                If there is a match the test will wait for live action on it
+                from user and when he finishes it will continue the test.
+            </Typography>
+            <br/>
+          <Button variant="outlined" color="primary">Open Static Masking wizard</Button>
+        </div>
         </AccordionDetails>
       </Accordion>
       </div>
