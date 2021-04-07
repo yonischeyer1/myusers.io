243caa678aa196d28b8c6339d20013dcab5b1a1f
yonis <yoni.scheyer@gmail.com>
2021-04-07 21:12:12 +0300

    wip export

---
+++ GUI/app/utils/exportImport.ts
@@ -0,0 +1,17 @@
+export function exportTestSuite (testSuite:any) {
+
+}
+
+export function importTestSuite (pathToGzipTestSuite:string) {
+
+}
+
+
+export function compress () {
+
+}
+
+
+export function decompress () {
+    
+}
